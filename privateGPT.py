ed4d110589e9b350f543c5ba159717ab7e714e63
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2023-07-20 22:23:33 +0930

    Unfinished changes to OpenAI cases

--- privateGPT.py
+++ privateGPT.py
@@ -4,10 +4,12 @@ from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import GPT4All, LlamaCpp, OpenAI
+from langchain.chat_models import ChatOpenAI
 import os
 import argparse
 import time
+import openai
 
 load_dotenv()
 
@@ -19,6 +21,7 @@ model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 from constants import CHROMA_SETTINGS
 
@@ -36,9 +39,13 @@ def main():
             llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case "GPT4All":
             llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+        case "OpenAI":
+            llm = OpenAI(model=model_path, n_ctx=model_n_ctx, backend='openai', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+        case "ChatOpenAI":
+            llm = ChatOpenAI(model=model_path, n_ctx=model_n_ctx, backend='openai', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
             # raise exception if model_type is not supported
-            raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+            raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All, OpenAI, ChatOpenAI")
         
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
     # Interactive questions and answers
